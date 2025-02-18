75e95634cc568fe5f1b2ed52916d77f6888c2965
NajiAbdlhag <najiabdlhag@gmail.com>
2025-02-18 15:29:24 +0300

    الواجب
    الواجب الثاني

--- lib/hoame.dart
+++ lib/hoame.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home > {
               children: [
                 buildRow(),
                 NewWidget2(),
-                buildRow1(),
+                NewWidget3(),
               ],
             ),
           ),
@@ -48,110 +48,159 @@ class _HomeState extends State<Home > {
     );
   }
 
-  Row buildRow1() {
-    return buildRow2();
-  }
-
-  Row buildRow2() {
-    return buildRow3();
-  }
-
-  Row buildRow3() {
-    return Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    color: Colors.brown,
-                    height: 40,
-                    width: 40,
-                  ),
-                  NewWidget1(),
-                  Container(
-                    color: Colors.blue,
-                    height: 40,
-                    width: 40,
-                  ),
-                  NewWidget(),
-                  Container(
-                    color: Colors.green,
-                    height: 40,
-                    width: 40,
-                  ),
-                  Divider(),
-                  buildRow4(),
-                  Divider() ,
-                  Text('RAFAT Ali'),
-                ],
-              ),
-            ],
-          );
-  }
-
-  Row buildRow4() {
-    return buildRow5();
-  }
-
-  Row buildRow5() {
-    return Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.purple,
-                    radius: 100,
-                    child: Stack(
-                      children: [
-                        Container(
-                          color: Colors.yellowAccent,
-                          height: 40,
-                          width: 40,),
-                        Container(
-                          color: Colors.blueGrey,
-                          height: 60,
-                          width: 60,),
-                        Container(
-                          color: Colors.red ,
-                          height: 100,
-                          width: 100,),
-
-
-
-
-                      ],
-                    ),
-                  ),
-                ],
-              );
-  }
-
   Row buildRow() {
-    return buildRow6();
+    return NewWidget4();
   }
+}
 
-  Row buildRow6() {
-    return buildRow7();
+class NewWidget4 extends StatelessWidget {
+  const NewWidget4({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NewWidget5();
   }
+}
 
-  Row buildRow7() {
+class NewWidget5 extends StatelessWidget {
+  const NewWidget5({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Row(
-  children: [
-    Container(
-      color: Colors.black,
-      height: 40,
-      width: 40,
-    ),
+      children: [
+        Container(
+          color: Colors.black,
+          height: 40,
+          width: 40,
+        ),
 
-    Padding(padding:  EdgeInsets .all( 16)),
-    Expanded(child:  Container(),),
-    Padding(padding:  EdgeInsets .all( 16)),
-    Container(
-      color: Colors.yellow,
-      height: 40,
-      width: 40,
-    ),
-    buildContainer(40, 180, Colors.blue,)
-  ],
+        Padding(padding:  EdgeInsets .all( 16)),
+        Expanded(child:  Container(),),
+        Padding(padding:  EdgeInsets .all( 16)),
+        Container(
+          color: Colors.yellow,
+          height: 40,
+          width: 40,
+        ),
+        buildContainer(40, 180, Colors.blue,)
+      ],
 
-);
+    );
+  }
+}
+
+class NewWidget3 extends StatelessWidget {
+  const NewWidget3({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NewWidget6();
+  }
+}
+
+class NewWidget6 extends StatelessWidget {
+  const NewWidget6({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NewWidget8();
+  }
+}
+
+class NewWidget8 extends StatelessWidget {
+  const NewWidget8({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Container(
+              color: Colors.brown,
+              height: 40,
+              width: 40,
+            ),
+            NewWidget1(),
+            Container(
+              color: Colors.blue,
+              height: 40,
+              width: 40,
+            ),
+            NewWidget(),
+            Container(
+              color: Colors.green,
+              height: 40,
+              width: 40,
+            ),
+            Divider(),
+            NewWidget7(),
+            Divider() ,
+            Text('RAFAT Ali'),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class NewWidget7 extends StatelessWidget {
+  const NewWidget7({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NewWidget9();
+  }
+}
+
+class NewWidget9 extends StatelessWidget {
+  const NewWidget9({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.purple,
+          radius: 100,
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.yellowAccent,
+                height: 40,
+                width: 40,),
+              Container(
+                color: Colors.blueGrey,
+                height: 60,
+                width: 60,),
+              Container(
+                color: Colors.red ,
+                height: 100,
+                width: 100,),
+
+
+
+
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
