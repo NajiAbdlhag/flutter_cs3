8a777640a48b1882f795eff8af094ca9870e0a37
NajiAbdlhag <najiabdlhag@gmail.com>
2025-03-03 16:19:30 +0300

    الواجب
    الاخير

--- lib/hoame.dart
+++ lib/hoame.dart
@@ -1,245 +1,73 @@
 import 'package:flutter/material.dart';
-class MyContainers extends StatefulWidget {
-  const MyContainers({super.key});
 
-  @override
-  State<MyContainers> createState() => _MyContainersState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyContainersState extends State<MyContainers> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterApp(),
+    );
   }
 }
 
-class Home  extends StatefulWidget {
-  const Home ({super.key});
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
 
   @override
-  State<Home > createState() => _HomeState();
+  State<CounterApp> createState() => _CounterAppState();
 }
 
-class _HomeState extends State<Home > {
-  final myContainer = Container(
-    color: Colors.black,
-    height: 40,
-    width: 40,
-  );
+class _CounterAppState extends State<CounterApp> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Flutter Counter App'),
       ),
-      body: SafeArea (
-        child: SingleChildScrollView(
-          child:Padding(padding: EdgeInsets .all( 16),
-            child :Column(
-              children: [
-                buildRow(),
-                NewWidget2(),
-                NewWidget3(),
-              ],
-            ),
-          ),
-        ),
+      body: counterWidget(),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          floatingActionButtonMethod(Icons.add, _incrementCounter),
+          SizedBox(width: 10),
+          floatingActionButtonMethod(Icons.add, _decrementCounter),
+        ],
       ),
     );
   }
 
-  Row buildRow() {
-    return NewWidget4();
+  Center counterWidget() {
+    return Center(
+      child: Text(
+        'Counter: $_counter',
+        style: TextStyle(fontSize: 32),
+      ),
+    );
   }
-}
 
-class NewWidget4 extends StatelessWidget {
-  const NewWidget4({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return NewWidget5();
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
   }
-}
 
-class NewWidget5 extends StatelessWidget {
-  const NewWidget5({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          color: Colors.black,
-          height: 40,
-          width: 40,
-        ),
-
-        Padding(padding:  EdgeInsets .all( 16)),
-        Expanded(child:  Container(),),
-        Padding(padding:  EdgeInsets .all( 16)),
-        Container(
-          color: Colors.yellow,
-          height: 40,
-          width: 40,
-        ),
-        buildContainer(40, 180, Colors.blue,)
-      ],
-
+  FloatingActionButton floatingActionButtonMethod(
+      IconData icon, VoidCallback onPressed) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: Icon(icon),
     );
   }
 }
-
-class NewWidget3 extends StatelessWidget {
-  const NewWidget3({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return NewWidget6();
-  }
-}
-
-class NewWidget6 extends StatelessWidget {
-  const NewWidget6({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return NewWidget8();
-  }
-}
-
-class NewWidget8 extends StatelessWidget {
-  const NewWidget8({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Column(
-          children: [
-            Container(
-              color: Colors.brown,
-              height: 40,
-              width: 40,
-            ),
-            NewWidget1(),
-            Container(
-              color: Colors.blue,
-              height: 40,
-              width: 40,
-            ),
-            NewWidget(),
-            Container(
-              color: Colors.green,
-              height: 40,
-              width: 40,
-            ),
-            Divider(),
-            NewWidget7(),
-            Divider() ,
-            Text('RAFAT Ali'),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-class NewWidget7 extends StatelessWidget {
-  const NewWidget7({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return NewWidget9();
-  }
-}
-
-class NewWidget9 extends StatelessWidget {
-  const NewWidget9({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CircleAvatar(
-          backgroundColor: Colors.purple,
-          radius: 100,
-          child: Stack(
-            children: [
-              Container(
-                color: Colors.yellowAccent,
-                height: 40,
-                width: 40,),
-              Container(
-                color: Colors.blueGrey,
-                height: 60,
-                width: 60,),
-              Container(
-                color: Colors.red ,
-                height: 100,
-                width: 100,),
-
-
-
-
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class NewWidget2 extends StatelessWidget {
-  const NewWidget2({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding:  EdgeInsets .all( 16.0),);
-  }
-}
-
-class NewWidget1 extends StatelessWidget {
-  const NewWidget1({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding:  EdgeInsets .all( 16.0),);
-  }
-}
-
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding:  EdgeInsets .all( 16.0),);
-  }
-}
-Container buildContainer(double x,double y,Color c){
-  return Container(
-    color: c,
-    height: x,
-    width: y,);
-}
-
